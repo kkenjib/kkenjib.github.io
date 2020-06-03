475f9db71b436c3c948e403e03f5502701d67b54
kkenjib <kbierig@email.arizona.edu>
2020-06-02 20:10:00 -0700

    Updates

---
+++ precache-manifest.138ea1da077f775c063a19f1e0e18f67.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b347c34b519389ed7b255f67cec4f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d18de0b013aa03adeb5b",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a941f61c453655bf9d55",
+    "url": "/static/css/main.28434790.chunk.css"
+  },
+  {
+    "revision": "d18de0b013aa03adeb5b",
+    "url": "/static/js/2.d56a1f39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d56a1f39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a941f61c453655bf9d55",
+    "url": "/static/js/main.7eff92b4.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "658d28748a393e07ae6d72e05390b528",
+    "url": "/static/media/logo.658d2874.png"
+  },
+  {
+    "revision": "30187ab7bcc3daa66655239ad4874b84",
+    "url": "/static/media/react-logo.30187ab7.jpg"
+  },
+  {
+    "revision": "f0e305c35dc666e932c868aed807db7f",
+    "url": "/static/media/treemap.f0e305c3.png"
+  }
+]);
